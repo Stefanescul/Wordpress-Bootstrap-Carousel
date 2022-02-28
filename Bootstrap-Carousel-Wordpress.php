3a9e569f337715a3dbe545b82f5a940bde30185b
Stefanescul <m.stefanescumihai@gmail.com>
2022-02-28 17:27:57 +0200

    Upload the code file

---
+++ Bootstrap-Carousel-Wordpress.php
@@ -0,0 +1,37 @@
+<?php $args = array(
+                 'post_type' => 'post',
+                'posts_per_page' => -1,
+                  
+                
+             );
+             $slider = new WP_Query($args);
+             if($slider->have_posts()):
+             
+             ?>
+
+<div class="container ">
+<div class="row ">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+
+
+                <div class="carousel-inner">
+                   <?php $i = 0; while($slider->have_posts()): $slider->the_post(); ?>
+                       <div class=" item carousel-item <?php echo ($i == 0) ? 'active' : ''?>">
+                             <?php the_post_thumbnail('post',  array(
+                                                                     'class' => 'd-block w-25 ', 
+                                                           
+                                                                      ) ) ; ?>
+                            
+                       </div><!--.carousel-item-->
+                   <?php $i++; endwhile; ?>
+                 
+
+                </div> <!--.carouse-inner-->
+
+
+</div> 
+ <?php endif; 
+        wp_reset_postdata(); ?>
+             
+          </div>
+        </div>
